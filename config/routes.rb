63ec51157841277b8cf60bc789649c4447fa07a4
Armando <aalves.job@gmail.com>
2016-11-01 23:44:32 -0200

    rails generate knock:token_controller user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
